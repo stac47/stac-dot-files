d46a1f1b5ff79ec664861063f7412937d03d6df0
Laurent Stacul <laurent.stacul@amadeus.com>
2020-09-24 15:17:51 +0000

    [Global] Add fast installation script

---
+++ install-tools.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+function install_tools() {
+    local os="$(uname)"
+    case ${os} in
+        Darwin)
+            _install_tools_darwin
+            ;;
+        Linux)
+            _install_tools_linux
+            ;;
+        *)
+            echo "Unknown OS"
+            exit 1
+            ;;
+    esac
+}
+
+function _install_tools_darwin() {
+    echo "TODO"
+    exit 1
+}
+
+function _install_tools_linux() {
+    local distrib=$(lsb_release -d | awk -F"\t" '{print $2}' | cut -d' ' -f 1)
+    case ${distrib} in
+        Ubuntu | Debian)
+            apt update && \
+            apt install -y \
+                apt-transport-https \
+                autoconf \
+                automake \
+                build-essential \
+                bzip2 \
+                ca-certificates \
+                chrpath \
+                cscope \
+                curl \
+                dos2unix \
+                exuberant-ctags \
+                gcc-doc \
+                gdb \
+                gdb-doc \
+                glibc-doc \
+                gnupg-agent \
+                htop \
+                jq \
+                libtool \
+                make-doc \
+                python3 \
+                python3-venv \
+                software-properties-common \
+                valgrind \
+                vim \
+                silversearcher-ag \
+                tmux \
+                tree \
+                zsh
+            ;;
+        *)
+            echo "Unknown Linux distribution"
+            exit 1
+            ;;
+    esac
+
+}
+
+install_tools
