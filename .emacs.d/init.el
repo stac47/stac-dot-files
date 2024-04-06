3422e663e6ff2e8ab675a4d71ffcb2b48d4f1b1b
Laurent Stacul <laurent.stacul@gmail.com>
2024-04-06 06:42:13 +0200

    [EMACS] Add yasnippet package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,16 +1,28 @@
-(menu-bar-mode -1)
-
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(yaml-mode markdown-mode)))
+ '(package-selected-packages '(yasnippet yaml-mode markdown-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Do not display the menu bar
+(menu-bar-mode -1)
+
+;; Automatically refresh file on change
+(global-auto-revert-mode t)
+
+;; In programming language mode, display the line number
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+;; Configure yasnippet
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"))
+(yas-reload-all)
+(add-hook 'prog-mode-hook #'yas-minor-mode)
