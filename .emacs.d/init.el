46032f2c6f32bed55ad4647d280f13c8ca1fbad9
Laurent Stacul <laurent.stacul@gmail.com>
2024-04-06 18:55:17 +0200

    [EMACS] Force final newline

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,23 @@
+;; Always add a newline character at the end of the file
+(setq require-final-newline t)
+
+;; Do not display the menu bar
+(menu-bar-mode -1)
+
+;; Automatically refresh file on change
+(global-auto-revert-mode t)
+
+;; In programming language mode, display the line number
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(add-hook 'LilyPond-mode-hook #'display-line-numbers-mode)
+
+;; Configure yasnippet
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"))
+(yas-reload-all)
+(add-hook 'prog-mode-hook #'yas-minor-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -10,19 +30,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;; Do not display the menu bar
-(menu-bar-mode -1)
-
-;; Automatically refresh file on change
-(global-auto-revert-mode t)
-
-;; In programming language mode, display the line number
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-
-;; Configure yasnippet
-(require 'yasnippet)
-(setq yas-snippet-dirs
-      '("~/.emacs.d/snippets"))
-(yas-reload-all)
-(add-hook 'prog-mode-hook #'yas-minor-mode)
