4cdccfa6f20164e6ae41447d44fde2e399f06e3e
Laurent Stacul <laurent.stacul@gmail.com>
2024-04-21 15:35:54 +0200

    [EMACS] Add 'magit'

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,9 +3,6 @@
 (add-to-list 'package-archives
              '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
 
 ;; Always add a newline character at the end of the file
 (setq require-final-newline t)
@@ -36,7 +33,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(flycheck yasnippet yaml-mode markdown-mode)))
+ '(package-selected-packages '(magit flycheck yasnippet yaml-mode markdown-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
