ec83122f89b97c64ff0487396242278ff7d19bf4
Laurent Stacul <laurent.stacul@gmail.com>
2024-04-28 16:31:34 +0200

    [EMACS] Use direnv

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -60,12 +60,16 @@
   (setq rbenv-show-active-ruby-in-modeline nil)
   (global-rbenv-mode))
 
+(use-package direnv
+  :config
+  (direnv-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(yasnippet yaml-mode markdown-mode magit flycheck)))
+ '(package-selected-packages '(direnv yasnippet yaml-mode markdown-mode magit flycheck)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
