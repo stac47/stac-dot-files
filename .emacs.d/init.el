6bf747a310922cd9e827267e8b629554fe3dedb7
Laurent Stacul <laurent.stacul@gmail.com>
2024-04-28 16:22:30 +0200

    [EMACS] Use rbenv for Ruby

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -54,3 +54,21 @@
   :config
   (add-hook 'markdown-mode-hook 'auto-fill-mode)
   (add-hook 'markdown-mode-hook (lambda() (set-fill-column 80))))
+
+(use-package rbenv
+  :config
+  (setq rbenv-show-active-ruby-in-modeline nil)
+  (global-rbenv-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(yasnippet yaml-mode markdown-mode magit flycheck)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
