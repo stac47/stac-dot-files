b5247ef50b892fe55f0c910ac75927e1f0ac41a6
Laurent Stacul <laurent.stacul@gmail.com>
2024-05-02 18:49:27 +0200

    [EMACS] Use melpa instead of stable melpa

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,8 +21,8 @@
 ;; Package system initializations
 (require 'package)
 
-(add-to-list 'package-archives
-             '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -85,7 +85,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ruby-end direnv yasnippet yaml-mode markdown-mode magit flycheck)))
+   '(ruby-end yasnippet yaml-mode markdown-mode magit flycheck)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
