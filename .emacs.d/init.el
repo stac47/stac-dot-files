b66850cb8358d77ed97e4f025eed923c9503419b
Laurent Stacul <laurent.stacul@gmail.com>
2024-05-03 09:56:37 +0200

    [EMACS] Add Melpa stable archive (just in case)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,7 +22,7 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
