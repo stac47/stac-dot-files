0370cfe545a96f5970b8996fce5eb20722b597e4
Laurent Stacul <laurent.stacul@gmail.com>
2024-05-16 20:28:36 +0200

    [EMACS] Remove long lines from init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,8 +30,10 @@
 ;; Package system initializations
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
